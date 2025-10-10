0730b8cd9a841581925e9daa1b120b7635a19060
JoaoHS2212 <joaohs2212@gmail.com>
2025-10-09 23:17:50 -0300

    Dia 5: Decifrador bidirecional completo no cifrador (shift negativo + teste) + TryHackMe room 2

--- cifrador_simples.py
+++ cifrador_simples.py
@@ -1,14 +1,30 @@
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
-shift = 5
-palavra = 'waka'  # Teste: vira 'ndnd'
+shift = 3
 
-cifrado = ''
-for char in palavra:
-    if char in alphabet:
-        indice = alphabet.index(char)
-        novo_indice = (indice + shift) % 26
-        cifrado += alphabet[novo_indice]
-    else:
-        cifrado += char
-print(f'Cifrado: {cifrado}')
-print("Wakanda PR Test!")
+def cifrar(palavra, shift=3):
+    cifrado = ''
+    for char in palavra:
+        if char in alphabet:
+            indice = alphabet.index(char)
+            novo_indice = (indice + shift) % 26
+            cifrado += alphabet[novo_indice]
+        else:
+            cifrado += char
+    return cifrado
+
+def decifra(palavra, shift=-3):
+    decifrado = ''
+    for char in palavra:
+        if char in alphabet:
+            indice = alphabet.index(char)
+            novo_indice = (indice + shift) % 26
+            decifrado += alphabet[novo_indice]
+        else:
+            decifrado += char
+    return decifrado
+
+
+palavra = 'waka'
+cifrado_test = cifrar(palavra)
+print(f'Cifrado: {cifrado_test}') 
+print(f'Decifrado: {decifra(cifrado_test)}') 
